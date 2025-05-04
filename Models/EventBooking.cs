fd2052e39f990304ce382d8263da0bfb26a1dfa7
Yara Emad Eldien <143430513+yaraemad4869@users.noreply.github.com>
2025-05-05 00:15:57 +0300

    ADD Booking.cs

---
+++ Models/EventBooking.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SCMS.Models
+{
+    public class EventBooking
+    {
+        public int Id { get; set; }
+        [ForeignKey("Event")]
+        public int EventId { get; set; }
+        public Event Event { get; set; }
+        [ForeignKey("Student")]
+        public int StudentId { get; set; } // User ID of student
+        public Student Student { get; set; }
+        public DateTime BookingDate { get; set; } = DateTime.Now;
+        public bool IsCancelled { get; set; } = false;
+    }
+}
