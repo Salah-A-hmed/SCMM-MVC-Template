b184d86b4eab3f0d404e556856e081f3558e3a2a
Yara Emad Eldien <143430513+yaraemad4869@users.noreply.github.com>
2025-05-05 00:20:23 +0300

    ADD EventsRepos

---
+++ IRepo/IEventBookingRepo.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using SCMS.Models;
+
+namespace SCMS.IRepo
+{
+
+    public interface IEventBookingRepo : IBasicRepo<EventBooking>
+    {
+        Task<IEnumerable<EventBooking>> GetBookingsByStudentAsync(int studentId, params Func<IQueryable<EventBooking>, IIncludableQueryable<EventBooking, object>>[] includes);
+        Task<EventBooking> GetBookingByEventAndStudentAsync(int eventId, int studentId);
+    }
+}
