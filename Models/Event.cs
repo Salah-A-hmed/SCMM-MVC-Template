918c9c7816ed4d7c58c501db226a69a2475ae8f3
Yara Emad Eldien <143430513+yaraemad4869@users.noreply.github.com>
2025-05-05 00:14:51 +0300

    Update Event.cs

--- Models/Event.cs
+++ Models/Event.cs
@@ -1,7 +1,35 @@
-﻿namespace SCMM.Models
+// Models/Event.cs
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SCMS.Models
 {
     public class Event
     {
-        // TODO: ID, Title, Description, Date, NotificationsEnabled
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+
+        [Required]
+        public DateTime StartDate { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue)]
+        public int Capacity { get; set; }
+        public int RegisteredCount { get; set; } = 0; // Number of students registered for the event
+        public bool IsActive { get; set; } = true;
+
+        //public int CreatedBy { get; set; } // User ID of creator
+
+        public ICollection<EventBooking> Bookings { get; set; }
     }
 }
