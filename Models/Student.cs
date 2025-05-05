a2ef9c4ccaf5713bb4a7594215515ebcedf6a451
Maiar-Hamdy <143446439+Maiar-Hamdy@users.noreply.github.com>
2025-05-05 15:39:15 +0300

    Student_Data

--- Models/Student.cs
+++ Models/Student.cs
@@ -1,7 +1,31 @@
-﻿namespace SCMM.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Security.Cryptography.X509Certificates;
+
+namespace SCMM.Models
 {
     public class Student
     {
         // TODO: ID, Name, Age, Email, AcademicYear, ImagePath, etc.
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        public int NationalId { get; set; }
+
+        [Required]
+        public int Age { get; set; }
+
+        [Required]
+        public string AcademicYear { get; set; }
+
+        public int GroupId { get; set; }
+
+        public ICollection<SupportTicket> SupportTickets { get; set; }
     }
 }
