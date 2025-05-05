ec996e7243c5695df8adc3ad13b0eccceafa6672
Maiar-Hamdy <143446439+Maiar-Hamdy@users.noreply.github.com>
2025-05-05 15:43:50 +0300

    Employee_Data

---
+++ Models/Employee.cs
@@ -0,0 +1,10 @@
+﻿namespace SCMM.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public ICollection<SupportTicket> SupportTickets { get; set; }
+
+    }
+}
