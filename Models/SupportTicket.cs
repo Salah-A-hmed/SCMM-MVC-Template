128cc71a4173ca10cb026ed0dc48c42d05e54b40
Maiar-Hamdy <143446439+Maiar-Hamdy@users.noreply.github.com>
2025-05-05 15:45:29 +0300

    Complaints_model

--- Models/SupportTicket.cs
+++ Models/SupportTicket.cs
@@ -1,7 +1,39 @@
-﻿namespace SCMM.Models
+﻿using SCMM.Data;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SCMM.Models
 {
     public class SupportTicket
     {
         // TODO: ID, StudentID, Title, Description, Status, Response
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Content { get; set; }
+
+        [Required]
+        public Categories Category { get; set; }
+
+        [Required]
+        public Complaints_status Status { get; set; } = Complaints_status.Pending;
+
+        public DateTime DateTime { get; set; } = DateTime.Now;
+
+       
+        
+        [Required]
+        public int StudentId { get; set; }
+
+        [ForeignKey("StudentId")]
+        public Student Student { get; set; }
+        public string? Answer { get; set; }
+
     }
 }
